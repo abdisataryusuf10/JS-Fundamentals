a960863bf659698407d90f2671041f4e730c9b65
Abdisatar Yusuf <satexza12@gmail.com>
2025-09-16 15:09:20 +0300

    2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,7 +1,11 @@
-const if (args.length ===0) {
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+
+if (args.length === 0) {
   console.log("No argument");
-} else if(arg.length ===1) {
-  console.log("Argument found")
-} else (args.length ===3) {
+} else if (args.length === 1) {
+  console.log("Argument found");
+} else {
   console.log("Arguments found");
 }
